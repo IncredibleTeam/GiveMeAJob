66ef5ee4ab9785e7a2e3f9e3984789312b0383ac
SergioAnd95 <developinc@yadnex.ru>
2017-09-19 13:27:56 +0300

    fix urls

--- givemeajob/urls.py
+++ givemeajob/urls.py
@@ -21,5 +21,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', TemplateView.as_view(template_name='index.html'), name='index')
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='index')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
